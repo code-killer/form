6f79d39eab6cf081e5f578eb7f573362f3474411
Rasbin Rijal <rasbin4brt@gmail.com>
2017-03-01 18:59:19 +0200

    Added various input types generator based on user's click

--- form.php
+++ form.php
@@ -25,18 +25,34 @@ else "no item id";
 <html>
 <head>
 <link rel=stylesheet type=text/css href=main.css>
-
-
 </head>
 <body>
 <div id="leftcol" >
 <?php
 // call the fielitem list funtion here
-$dyn_table = '<tr><td>' . '<a style="text-decoration:none; height:60px; width:90px; display:block; text-align:center; background-color:#2E59A4; color:#FFFFFF;" href="form.php?itemid=1">Text Filed</a>' . '</td>';
+$dyn_table = '<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;  padding-top: 10px;"  href="form.php?itemid=1">Text Field</a>'
+			      . '</td>
+				  </tr></br>' . 
+			'<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;  padding-top: 10px;"  href="form.php?itemid=6">Email Field</a>'
+			      . '</td>
+				  </tr></br>' .	  
+			 '<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;   padding-top: 10px;" href="form.php?itemid=2">Radio Button</a>'
+			      . '</td></tr></br>' . 	  
+			 '<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;   padding-top: 10px;" href="form.php?itemid=3">Check Box</a>'
+			      . '</td></tr></br>' . 
+			'<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;   padding-top: 10px;" href="form.php?itemid=4">Text Area</a>'
+			      . '</td></tr></br>' .
+			'<tr><td>' . '<a style="text-decoration:none; height:30px; width:100px; display:block; text-align:center;
+                              background-color:#2E59A4; color:#FFFFFF;   padding-top: 10px;" href="form.php?itemid=5">Button</a>'
+			      . '</td></tr></br>'	  
+				  ;
 echo $dyn_table;
-//require "table1cart.php";
-//include ('connection.php');
-//cart();
+
 
 ?>
 </div><!--end of leftcol div-->
@@ -45,14 +61,45 @@ echo $dyn_table;
 
 <?php
 if(isset($itemid)) {
-$dyn_table = '<tr><td>' . '<input type="text" style="text-decoration:none; height:30px; width:200px; display:block; text-align:center; background-color:#2E59A4; color:#FFFFFF;" />' . '</td>';
+	if($itemid==1) {
+$dyn_table = '<tr>
+                <td>' . '<input type="text" value="Text goes here" style="text-decoration:none; height:30px; width:200px; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF; padding-top: 10px;" />' . '</td>
+	</tr></br>'; }
+	if($itemid==2) {
+$dyn_table =
+			  '<tr>
+                <td>' . '<input type="radio" style="text-decoration:none; height:20px; width:200px; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF; padding: 10px 0;" />' . '</td>
+	</tr></br>' ; } 
+if($itemid==3) {
+$dyn_table =			  '<tr>
+                <td>' . '<input type="checkbox" style="text-decoration:none; height:20px; width:200px; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF;  padding-top: 10px;" />' . '</td>
+</tr></br>' ; } 
+if($itemid==4) {
+$dyn_table =			  '<tr>
+                <td>' . '<textarea rows="5" cols="50" style="text-decoration:none; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF;  padding-top: 10px;" >Enter your text here.</textarea>' . '</td>
+</tr></br>' ; }
+if($itemid==5) {
+$dyn_table =			  '<tr>
+                <td>' . '<input type="button" value="SUBMIT" style="text-decoration:none; height:30px; width:100px; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF;  padding-top: 10px;" >' . '</td>
+</tr></br>'; }
+if($itemid==6) {
+$dyn_table = '<tr>
+                <td>' . '<input type="email" value="Type your email" style="text-decoration:none; height:30px; width:200px; display:block;
+                                text-align:center; background-color:#2E59A4; color:#FFFFFF; padding-top: 10px;" />' . '</td>
+	</tr></br>'; }
+			  
 echo $dyn_table;
-echo "here we come";
+
 }
 cart();
 //call the display form function
 //displayitem ();
-echo "hello ";
+
 ?>
 </div><!--end of rightcol div-->
 </body>
